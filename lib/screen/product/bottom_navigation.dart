052b8f53776de7099dd5d391c3dd89a2f8154bbf
Jumvi <judahmvi@gmail.com>
2024-08-28 14:50:03 +0100

    init bottom widget

---
+++ lib/screen/product/bottom_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class bottomWidget extends StatefulWidget {
+  const bottomWidget({super.key});
+
+  @override
+  State<bottomWidget> createState() => _bottomWidgetState();
+}
+
+class _bottomWidgetState extends State<bottomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
